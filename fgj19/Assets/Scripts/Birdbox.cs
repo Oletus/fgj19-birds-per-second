b208859baa3ba84ae2a1aa6a64780f750b923969
Olli Etuaho <olli.etuaho@gmail.com>
2019-01-25 22:23:00 +0200

    Add some placeholder scripts

---
+++ fgj19/Assets/Scripts/Birdbox.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdbox : MonoBehaviour
+{
+    public enum State
+    {
+        Detached,
+        PickedUp,
+        AttachedToTree
+    }
+
+    public bool OnLeftSideOfTree { set; get; }
+
+    private List<BirdboxSegment> Segments;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Segments = new List<BirdboxSegment>(GetComponentsInChildren<BirdboxSegment>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ fgj19/Assets/Scripts/BirdboxSegment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdboxSegment : MonoBehaviour
+{
+    [SerializeField] private int _Type;
+    public int Type { get { return _Type; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
