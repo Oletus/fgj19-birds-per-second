1cf53d5584308403c32c1dc34967c6762e857cfb
Olli Etuaho <olli.etuaho@gmail.com>
2019-01-26 17:50:26 +0200

    Small adjustments to test tree

--- fgj19/Assets/Scripts/RandomAlbedoOffset.cs
+++ fgj19/Assets/Scripts/RandomAlbedoOffset.cs
@@ -4,34 +4,24 @@ using UnityEngine;
 
 public class RandomAlbedoOffset : MonoBehaviour
 {
-    //public Texture main_Texture;
-    Renderer main_Renderer;
-
-    int random_Y_Tiling;
-    int x_Tiling;
-
-    int random_Y_Offset;
-    int x_Offset;
-
-    void Start()
+    void Awake()
     {
-        main_Renderer = GetComponent<Renderer>();
+        RandomizeTiling();
+    }
+
+    void RandomizeTiling()
+    {
+        Renderer renderer = GetComponent<Renderer>();
 
         //Tiling
-        x_Tiling = 1;
-        random_Y_Tiling = Random.Range(16, 30);
+        int x_Tiling = 1;
+        float random_Y_Tiling = Random.Range(0.6f, 1.0f) * transform.localScale.y;
 
         //Offset
-        x_Offset = 0;
-        random_Y_Offset = Random.Range(1, 1000);
+        int x_Offset = 0;
+        int random_Y_Offset = Random.Range(1, 1000);
 
-        //Enable keywords
-        //main_Renderer.material.EnableKeyword("_ALBEDO");
-
-        //Setting texture or albedo
-        //main_Renderer.material.SetTexture("_MainTex", main_Texture);
-
-        main_Renderer.material.mainTextureOffset = new Vector2(x_Offset, random_Y_Offset);
-        main_Renderer.material.mainTextureScale = new Vector2(x_Tiling, random_Y_Tiling);
+        renderer.material.mainTextureOffset = new Vector2(x_Offset, random_Y_Offset);
+        renderer.material.mainTextureScale = new Vector2(x_Tiling, random_Y_Tiling);
     }
 }
