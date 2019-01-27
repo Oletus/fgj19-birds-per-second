6092b47824b6009b2462d2abeea2923a8530346a
Olli Etuaho <olli.etuaho@gmail.com>
2019-01-27 12:26:14 +0200

    Some cinematics setup

---
+++ fgj19/Assets/Scripts/Utility/FixHorizontalFOV.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class FixHorizontalFOV : MonoBehaviour
+{
+    private Camera Camera;
+    private Cinemachine.CinemachineVirtualCamera VirtualCamera;
+
+    public float HorizontalFOV = 75.0f;
+
+    void Awake()
+    {
+        Camera = GetComponent<Camera>();
+        VirtualCamera = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+    }
+
+    void LateUpdate()
+    {
+        if (!Application.isPlaying)
+        {
+            Camera = GetComponent<Camera>();
+            VirtualCamera = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+        }
+        float hFOVrad = HorizontalFOV * Mathf.Deg2Rad;
+        float camHeight = Mathf.Tan(hFOVrad * .5f) / Camera.main.aspect;
+        float verticalFOVrad = Mathf.Atan(camHeight) * 2f;
+        if ( Camera )
+        {
+            Camera.fieldOfView = verticalFOVrad * Mathf.Rad2Deg;
+        }
+        if ( VirtualCamera )
+        {
+            VirtualCamera.m_Lens.FieldOfView = verticalFOVrad * Mathf.Rad2Deg;
+        }
+    }
+}
