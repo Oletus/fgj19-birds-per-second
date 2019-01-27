8b66fc0cfdba41f44280ab3f6f867747e3bd671e
Olli Etuaho <olli.etuaho@gmail.com>
2019-01-27 10:51:53 +0200

    Revert some of the randomization changes to fix gameplay

--- fgj19/Assets/Scripts/Player/PlacedObjectQueue.cs
+++ fgj19/Assets/Scripts/Player/PlacedObjectQueue.cs
@@ -5,43 +5,44 @@ using UnityEngine;
 public class PlacedObjectQueue : MonoBehaviour
 {
     public List<Birdhouse> PlacedObjects;
-    public List<Birdhouse> randomPlacedObjects;
-    public int birdhousesN;
+
+
+    [Header("Randomization")]
+    [SerializeField]
+    private int RandomBirdhousesN;
 
     [SerializeField]
-    public Birdhouse birdhouse;
+    private Birdhouse BirdhousePrefab;
 
     [SerializeField]
-    public BirdhouseSegment birdhouseSegment;
+    private BirdhouseSegment BirdhouseSegmentPrefab;
 
-    void Start() {
-        randomPlacedObjects = new List<Birdhouse>();
-
-        for (int i = 0; i < birdhousesN; i++)
+    [ContextMenu("Randomize")]
+    void Randomize() {
+        for (int i = 0; i < RandomBirdhousesN; i++)
         {
             var pos = new Vector3(Random.Range(-8F, -2F), 0, Random.Range(-8F, -2F));
-            Birdhouse bh = Instantiate(birdhouse, pos, Quaternion.Euler(0, 180, 0));
+            Birdhouse bh = Instantiate(BirdhousePrefab, pos, Quaternion.Euler(0, 180, 0));
 
             for (int j = 0; j < Random.Range(1, 3); j++)
             {
-                BirdhouseSegment bhSeg = Instantiate(birdhouseSegment, bh.transform);
+                BirdhouseSegment bhSeg = Instantiate(BirdhouseSegmentPrefab, bh.transform);
                 bhSeg.BaseColorIndex = Random.Range(1, 4);
             }
-
-            bh.Segments = new List<BirdhouseSegment>(bh.GetComponentsInChildren<BirdhouseSegment>());
-            randomPlacedObjects.Add(bh);
+            PlacedObjects.Add(bh);
         }
     }
 
     // Remove the next object from the queue and return it.
     public Birdhouse ObtainNext()
     {
-        if (randomPlacedObjects.Count > 0) {
-            var next = randomPlacedObjects[0];
-            randomPlacedObjects.RemoveAt(0);
-            return next;
+        if ( PlacedObjects == null || PlacedObjects.Count == 0 )
+        {
+            return null;
         }
 
-        return null;
+        var next = PlacedObjects[0];
+        PlacedObjects.RemoveAt(0);
+        return next;
     }
 }
