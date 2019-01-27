21e15e47630dae6c4192a9afc6f4dbd16968f474
Brandon <brandon.startz@gmail.com>
2019-01-27 15:38:52 +0200

    Flying birds

---
+++ fgj19/Assets/Flying_Bird_Random_Anim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flying_Bird_Random_Anim : MonoBehaviour
+{
+    int randomNum;
+    float newRandomNum;
+    Animator animator;
+
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        StartCoroutine("GetRandom");
+    }
+
+    public IEnumerator GetRandom()
+    {
+        randomNum = Random.Range(1, 10);
+        newRandomNum = randomNum * 1.0f;
+        //Debug.Log("waiting for... " + newRandomNum);
+
+        yield return new WaitForSeconds(newRandomNum);
+
+        // Debug.Log("should have called... " + newRandomNum);
+        animator.SetTrigger("Trigger");
+    }
+}
