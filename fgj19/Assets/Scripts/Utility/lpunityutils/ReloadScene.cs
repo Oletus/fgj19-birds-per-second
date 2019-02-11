f0d8c16b8938e5393c82d9449ae62d3f0969be06
Olli Etuaho <olli.etuaho@gmail.com>
2019-02-12 00:01:40 +0200

    Add UI to reload scene

---
+++ fgj19/Assets/Scripts/Utility/lpunityutils/ReloadScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LPUnityUtils
+{
+    public class ReloadScene : MonoBehaviour
+    {
+        public void ReloadCurrentScene()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        }
+    }
+}
